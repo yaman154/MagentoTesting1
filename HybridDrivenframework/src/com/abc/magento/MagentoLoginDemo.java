3dc929662680abdc1384581084415e5376c2fbc6
ABC FOR JAVA TESTING <ABC FOR JAVA TESTING@DESKTOP-M19IHU9>
2020-02-14 12:48:58 +0000

    first commit

---
+++ HybridDrivenframework/src/com/abc/magento/MagentoLoginDemo.java
@@ -0,0 +1,43 @@
+package com.abc.magento;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MagentoLoginDemo {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis = new FileInputStream("C:\\Users\\ABC FOR JAVA TESTING\\Desktop\\NovemberBatch1\\HybridDrivenframework\\src\\com\\abc\\utilities\\hybriddrivenframework.properties");
+		Properties p = new Properties();
+		p.load(fis);
+		
+	/* for loading the Data */
+	   String urlData = p.getProperty("url");
+	String usernameData = p.getProperty("username1");
+	 String passwordData = p.getProperty("password1");
+	
+	 /* for loading the Keywords */
+		String myacctKey = p.getProperty("myacct");
+		String usernameKey = p.getProperty("username");
+	String passwordKey = p.getProperty("password");
+	String loginKey = p.getProperty("login");
+	String logoutKey = p.getProperty("logout");
+	
+	WebDriver driver = new ChromeDriver();
+	
+	driver.get(urlData);
+	driver.findElement(By.xpath(myacctKey)).click();
+	driver.findElement(By.xpath(usernameKey)).sendKeys(usernameData);
+	driver.findElement(By.xpath(passwordKey)).sendKeys(passwordData);
+	driver.findElement(By.xpath(loginKey)).click();
+	driver.findElement(By.xpath(logoutKey)).click();
+	driver.quit();
+	}
+
+}
